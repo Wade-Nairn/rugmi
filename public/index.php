637451d5f8304e725e0516c41eb968ebe5a8cbfd
Michael Davies <mykudeez@gmail.com>
2015-06-16 17:26:29 +1200

    Landing page with loaded libs, models, and views (Need Logic and missing image_list.php)

--- public/index.php
+++ public/index.php
@@ -1,2 +1,25 @@
 <?php
 
+# index.php
+
+# 1. Load Libraries & models
+
+require_once '../libraries/auth.lib.php';
+require_once '../libraries/xss.lib.php';
+
+require_once '../models/user.model.php';
+require_once '../models/image.model.php';
+
+require_once '../models/images.collection.php';
+
+
+#2. Logic
+
+
+
+
+#3. Load Views
+
+include '../views/header.php';
+include '../views/image_list.php';
+include '../views/footer.php';
