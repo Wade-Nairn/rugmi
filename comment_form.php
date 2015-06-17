492ef75148883560ac7d8f5d5290541c74b7d1b0
marionrobles <marionroblesnz@gmail.com>
2015-06-17 16:05:13 +1200

    Create comment_form.php

---
+++ comment_form.php
@@ -0,0 +1,27 @@
+<?php 
+
+# comment_form.php
+
+<div class="main"> 
+  
+  <?= Form::open() ?>
+    
+    <div class="row">
+      
+      <?= Form::label('content', 'Content') ?>
+      <?= Form::textarea('content') ?>
+    
+    </div>
+    
+    <?= Form::submit() ?>
+  
+  <?= Form::close() ?>
+  
+  <? if($comment->id): ?>
+  
+    <a href="delete_comment.php?id=<?= $comment->id ?">>Delete</a>
+    <a href="edit_comment.php?id=<?= $comment->id ?">>Edit</a>
+  
+  <? endif; ?>
+  
+</div>
