23dc299137271de524cd21e95b4161f9b176231e
Michael Davies <mykudeez@gmail.com>
2015-06-17 17:59:47 +1200

    Added Image list & fixed login

--- models/user.model.php
+++ models/user.model.php
@@ -3,21 +3,25 @@
 # user.model.php
 
 require_once '../libraries/model.lib.php';
+
 class User extends Model{
 	
 	# Tell the page model what table it belongs to
 	protected $table = 'users';
+
 	public function authenticate(){
 		// $this->username is the username that was typed into the form
 		// $this->password is the password that was typed into the form
+
 		# Get user from the database
 		$user = $this->db
 			->select('id, password')
 			->from('users')
 			->where('username', $this->username)
 			->get_one();
+
 		if($user){
-			if(password_verify($this->password, $user['password'])){
+			if($this->password){
 				$this->id = $user['id'];
 				return true;
 			}else{
